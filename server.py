af98810d714e77df0e6c6ba35a487bd0b0ecb8cd
Gilberto Kreisler <gkreisler5@hotmail.com>
2019-12-01 16:11:19 -0300

    Incompleto

--- server.py
+++ server.py
@@ -25,70 +25,132 @@ server_socket.bind((bind_ip, bind_port))
 
 server_socket.listen()
 
-sockets_list = [server_socket]
+sockets_list = []
 
 clients_dict = {}
 
 print("[*] Listening on %s:%d" % (bind_ip,bind_port))
 
-def receive_message(client_socket):
-    try:
-        
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        if not len(message_header):
-            return False
-
-        message_length = int(message_header.decode('utf-8').strip())
-
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-
-    except:
-
-        return False
-
 def handle_client(client_socket):
-
-    #exibe o que o cliente quer enviar
-    request = client_socket.recv(1024)
-
-    print("[*] Received: %s" % request)
-
-    #envia pacote de volta
-    client_socket.send(bytes("ACK", "utf-8"))
-
-    client_socket.close()
-
-def main():
-
+    
     while True:
-
-        client,addr = server_socket.accept()
 
         try:
 
-            message_header = client.recv(HEADER_LENGTH)
+            message_header_rcv = client_socket.recv(HEADER_LENGTH).decode('utf-8').strip()
+
+            if not len(message_header_rcv):
+                #Cliente desconectou
+                del clients_dict[client_socket]
+                sockets_list.remove(client_socket)
+                client.close()
+
+                break
+
+            message_parse = message_header_rcv.split()
+            command = message_parse[0]
+
+            if command == LIST_USERS:
+                users_names = ""
+
+                for client in clients_dict:
+                    if client != client_socket:
+                        nick_string = clients_dict[client][nick] + "\n"
+                        users_names += nick_string
+
+                message_header = f"{len(users_names):<HEADER_LENGTH}".encode('utf-8')
+
+                client_socket.send(message_header + users_names)
+
+            elif command == NEW_NICK:
+
+                nick_length = int(message_parse[1])
+                
+                try:
+                    new_nick_try = client_socket.recv(nick_length).decode('utf-8')
+                    can_change = True
+
+                    for client in clients_dict:
+                        if client != client_socket:
+                            if clients_dict[client]['nick'] == new_nick_try:
+                                can_change = False
+                                break
+
+                    message_header = ""
+
+                    if can_change:
+                        message_header = f"{NICK_CHANGED:<{HEADER_LENGTH}}".encode('utf-8')
+                    
+                    else:
+                        message_header =f"{NICK_EXIST:<{HEADER_LENGTH}}".encode('utf-8')
+
+                    client_socket.send(message_header)
+
+                except:
+                    #deu algum problema com a conexão
+                    client_socket.close()
+                    break
+
+            elif command == MESSAGE:
+
+                message_size = int(message_parse[1])
+
+        except:
+            #ocorreu alguma desconexão
+            client_socket.close()
+            break 
+
+
+def main():
+    #Só vai emitir a thread de cliente se a conexão for bem sucedida
+    while True:
+
+        client_socket,addr = server_socket.accept()
+
+        try:
+
+            rcv_message_header = client_socket.recv(HEADER_LENGTH)
 
             if not len(message_header):
-                error_message = "Apelido obrigatório".encode('utf-8')
-                error_header = f"{len(error_message):<{HEADER_LENGTH}}"
-                client.send(error_header+error_message)
-
+                #continua no loop aguardando conexões
                 continue
 
-            nick_length = int(message_header.decode('utf-8').strip())
-            user_nick = client.recv(nick_length)
-            user_nick = user_nick.decode('utf-8')
+            
+            nick_length = int(rcv_message_header.decode('utf-8').strip())
+            user_nick = client_socket.recv(nick_length).decode('utf-8')
 
-            clients_dict[user_nick] = client
-            sockets_list.append(client)
+            if len(clients_dict):
+                nick_existent = False
+
+                for client in clients_dict:
+                    if clients_dict[client]['nick'] == user_nick:
+                        nick_existent = True
+                        break
+
+                if nick_existent:
+                    snd_message_header = f"{NICK_EXIST:<{HEADER_LENGTH}}"
+
+                    client_socket.send(snd_message_header)
+
+                    client_socket.close()
+
+                    continue
+
+
+            clients_dict[client_socket] = {'header': nick_length, 'nick': user_nick}
+            sockets_list.append(client_socket)
+
+            snd_message_header = f"{CONN_ACCEP:<{HEADER_LENGTH}}"
+
+            client_socket.send(snd_message_header)
 
             print("[*] Accepted connection from: %s:%d" % (addr[0],addr[1]))
 
-            client_handler = threading.Thread(target=handle_client, args=(client,))
+            client_handler = threading.Thread(target=handle_client, args=(client_socket,))
             client_handler.start()
 
         except:
             continue
+
 
 main()
